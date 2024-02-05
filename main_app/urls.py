ce8814277abf6687ffd8f1c6bc1134e96ab602b9
wysmith00 <wysmith00@gmail.com>
2024-02-05 16:12:36 -0500

    part 2 complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-# import Home view from the views file
-from .views import Home
+from .views import Home, BirdList, BirdDetail
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  # new routes below 
+  path('bird/', BirdList.as_view(), name='bird-list'),
+  path('birds/<int:id>/', BirdDetail.as_view(), name='bird-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import generics
+from .models import Bird
+from .serializers import BirdSerializer
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the bird-collector api home route!'}
     return Response(content)
+
+class BirdList(generics.ListCreateAPIView):
+  queryset = Bird.objects.all()
+  serializer_class = BirdSerializer
+
+class BirdDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Bird.objects.all()
+  serializer_class = BirdSerializer
+  lookup_field = 'id'
